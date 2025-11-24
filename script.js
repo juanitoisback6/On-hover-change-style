57c5bcf009413c4b83b4c2a9147c10e0eba286f2
Dakitidunno <juanchojuan6@hotmail.com>
2025-11-23 20:24:51 -0500

    I didn-t like the color, just change it

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ const box = document.getElementById("box");
 box.addEventListener("mouseover", ()=>
 {
 
-box.style.backgroundColor = "yellow";
+box.style.background = "radial-gradient(circle, #fff59d, #ffeb3b, #fbc02d, #f57f17)";
 }
 );
 box.addEventListener("mouseout", ()=>
 {
 
-box.style.backgroundColor = "white";
+box.style.background = "white";
           
 });
