1b7385c104f3d880112ad72455769403d028198a
Dakitidunno <juanchojuan6@hotmail.com>
2025-11-23 20:16:45 -0500

    Here there is an onhover that changes the stule of the div

---
+++ script.js
@@ -0,0 +1,20 @@
+const box = document.getElementById("box");
+
+
+
+
+
+
+
+box.addEventListener("mouseover", ()=>
+{
+
+box.style.backgroundColor = "yellow";
+}
+);
+box.addEventListener("mouseout", ()=>
+{
+
+box.style.backgroundColor = "white";
+          
+});
